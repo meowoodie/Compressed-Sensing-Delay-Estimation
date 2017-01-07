9c5e8f2001d7e1c231b90c261e47a35120c25c24
meowoodie <woodielove@163.com>
2017-01-06 21:16:42 -0500

    move all the matlab file into a folder

---
+++ matlab/signal_generator.m
@@ -0,0 +1,18 @@
+function [x1, x2] = signal_generator(time_ind, tau, period, Fs, sigma)
+
+    x1 = sin(2*pi*time_ind/period) + sigma.^2*randn(length(time_ind),1)';
+    x2 = sin(2*pi*(time_ind - tau)/period) + sigma.^2*randn(length(time_ind),1)';
+    
+    time_axis = linspace(0, length(time_ind) / Fs, length(time_ind));
+    freq_axis = linspace(-Fs/2, Fs/2, length(time_ind));
+    
+    f = figure;
+    subplot(4,1,1); plot(time_axis, x1, 'b'); xlabel('Time (s)'); title('x1 time domain');
+    subplot(4,1,2); plot(time_axis, x2, 'r'); xlabel('Time (s)'); title('x2 time domain');
+    subplot(4,1,3); plot(freq_axis, fftshift(abs(fft(x1))), 'b'); xlabel('Freq (hz)'); title('x1 freq domain');
+    subplot(4,1,4); plot(freq_axis, fftshift(abs(fft(x2))), 'r'); xlabel('Freq (hz)'); title('x2 freq domain');
+    myboldify(f);
+
+    x1 = x1';
+    x2 = x2';
+end
---
+++ matlab/signal_reader.m
@@ -0,0 +1,27 @@
+function [x1, x2] = signal_reader(path, x1_data, x2_data)
+%     path = '/Users/woodie/Desktop/Georgia-Tech-ISyE-Intern/time_delay_estimation/data/average_24hr_6v538-73392/';
+    formatSpec = '%f';
+    
+%     f1 = fopen([path '73392_0000.txt'],'r');
+    f1 = fopen([path x1_data],'r');
+    x1 = fscanf(f1,formatSpec);
+    x1 = normc(x1);
+    fclose(f1);
+
+%     f2 = fopen([path '73395_0000.txt'],'r');
+    f2 = fopen([path x2_data],'r');
+    x2 = fscanf(f2,formatSpec);
+    x2 = normc(x2);
+    fclose(f2);
+    
+    time_axis = linspace(0, 3600, length(x1));
+    freq_axis = linspace(-250, 250, length(x1));
+    
+    f = figure;
+    subplot(4,1,1); plot(time_axis, x1, 'b'); xlabel('Time (s)'); title('x1 time domain');
+    subplot(4,1,2); plot(time_axis, x2, 'r'); xlabel('Time (s)'); title('x2 time domain');
+    subplot(4,1,3); plot(freq_axis, fftshift(abs(fft(x1))), 'b'); xlabel('Freq (hz)'); title('x1 freq domain');
+    subplot(4,1,4); plot(freq_axis, fftshift(abs(fft(x2))), 'r'); xlabel('Freq (hz)'); title('x2 freq domain');
+    myboldify(f);
+end
+
