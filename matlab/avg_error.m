59487dee2128aa7834a9f5397557b03326cdfb8c
meowoodie <woodielove@163.com>
2017-01-22 11:29:31 -0500

    try more data in this exp

--- matlab/avg_error.m
+++ matlab/avg_error.m
@@ -76,7 +76,7 @@ for i = 1:300 %size(ind_pair_list, 2)
     tau0_ind = tau_xcorr/Ts; % The initial tau for the method 2
     acc      = 100;          % The accuracy of the downsampling
     times    = 3;            % The times of computation for one downsampling rate
-    ds_rates = linspace(0, 1, acc); ds_rates(1) = []; % Remove value 0
+    ds_rates = linspace(0, 1, acc);  ds_rates(1) = []; % Remove value 0
     tau_list = zeros(1, acc - 1);
     for k = 1:acc-1
         taus = zeros(1, times);
@@ -104,13 +104,13 @@ end
 % Compute the mean for each of the errors
 avg_err = mean(sum_err);
 
-% x_axis  = (1:length(tau_list)) / (acc/100);   % downsampling rate 0-100%
-% 
-% f = figure;
-% % subplot(2,1,1); plot(x_axis, tau_list, 'r', x_axis, real_tau, 'b'); xlabel('downsampling rate(%)'); ylabel('Tau (s)');
-% % subplot(2,1,2); 
-% plot(x_axis, avg_err); xlabel('downsampling rate(%)'); ylabel('Error (s)');
-% myboldify(f);
+x_axis  = (1:length(tau_list)) / (acc/100);   % downsampling rate 0-100%
+
+f = figure;
+% subplot(2,1,1); plot(x_axis, tau_list, 'r', x_axis, real_tau, 'b'); xlabel('downsampling rate(%)'); ylabel('Tau (s)');
+% subplot(2,1,2); 
+plot(x_axis, avg_err); xlabel('downsampling rate(%)'); ylabel('Error (s)');
+myboldify(f);
 
 save('error_over_downsampling.mat', 'sum_err', 'avg_err');
 
