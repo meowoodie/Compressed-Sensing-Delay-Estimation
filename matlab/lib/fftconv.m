3f5fe72d6676122f03e60d30aa290ebab79823a6
meowoodie <woodielove@163.com>
2017-06-15 20:45:24 +0800

    fix a bug

--- matlab/lib/fftconv.m
+++ matlab/lib/fftconv.m
@@ -1,19 +1,19 @@
 function tau = fftconv(x1, x2, Fs, low_freq, high_freq)
     %% Padding zeros
-    x1 = [x1' zeros(1, length(x1))];
-    x2 = [x2' zeros(1, length(x2))];
+    x1 = [x1 zeros(1, length(x1))];
+    x2 = [x2 zeros(1, length(x2))];
     n  = length(x1); % The length of the signal
 
     %% Do FFT for each of the signals
     x1_fft = fft(x1, n); % same size as x1
     x2_fft = fft(x2, n); % same size as x2
-
+    
 %     fig_1 = figure;
 %     subplot(2,1,1); plot(abs(x1_fft));
 %     subplot(2,1,2); plot(abs(x2_fft));
 
     %% apply bandpass filter
-    ideal_BP = filters.ideal_BP_filter(low_freq, high_freq, n, Fs);
+    ideal_BP = filters.gaussian_filter(low_freq, high_freq, 1, n, Fs);
 
     x1_filter = x1_fft .* ideal_BP;
     x2_filter = x2_fft .* ideal_BP;
@@ -26,14 +26,15 @@ function tau = fftconv(x1, x2, Fs, low_freq, high_freq)
     %% Cross-correlation by FFT -> convolution -> IFFT
     cross = conj(x1_filter) .* x2_filter;
     conv_f = fftshift(ifft(cross));
-        
-%     f2 = figure; 
-%     plot(real(conv_f)); 
-%     title('fftconv'); 
-%     ylabel('Amplitude');
-%     myboldify(f2);
+ 
+    f2 = figure; 
+    plot(real(conv_f)); 
+    title('fftconv'); 
+    ylabel('Amplitude');
+    myboldify(f2);
 
     %% Calculate the delay
     [m_value, m_index] = max(real(conv_f));
     tau = (m_index - n) / Fs;
+    disp(tau);
 end
