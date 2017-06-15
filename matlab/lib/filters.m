085646ddc6da663ad22a9643c0f4e03307e4127b
meowoodie <woodielove@163.com>
2017-06-15 17:53:31 +0800

    remove useless filters and add gaussian filter

--- matlab/lib/filters.m
+++ matlab/lib/filters.m
@@ -1,70 +1,52 @@
 classdef filters
-    methods(Static) 
-        function ideal_BP = ideal_BP_filter(low_freq, high_freq, n, Fs)
-            ind_low  = round(low_freq * n / Fs) + 1;
-            ind_high = round(high_freq * n / Fs) + 1;
-            ideal_BP = zeros(1, n);
+    methods(Static)
+        function filter = ideal_filter(...
+                low_freq, high_freq, ... % The range of band pass
+                downsample_rate, ...     % downsampling rate
+                n, Fs)
+            % Index of low frequency and high frequency in the positive
+            % frequency domain
+            ind_low  = round(low_freq * n / Fs);
+            ind_high = round(high_freq * n / Fs);
+            filter = zeros(1, n);
             % Positive freq part
-            ideal_BP(ind_low:ind_high - 1) = ones(1, ind_high - ind_low); 
+            filter(ind_low:ind_high - 1) = ones(1, ind_high - ind_low); 
             % Negative freq part
-            ideal_BP(n - ind_high + 1:n - ind_low) = ones(1, ind_high - ind_low); 
-        end
-        function ideal_BP = ideal_BP_DS_filter(low_freq, high_freq, downsample_rate, n, Fs)
-            ind_low  = round(low_freq * n / Fs) + 1;
-            ind_high = round(high_freq * n / Fs) + 1;
-            ideal_BP = zeros(1, n);
-            % Positive freq part
-            ideal_BP(ind_low:ind_high - 1) = ones(1, ind_high - ind_low); 
-            % Negative freq part
-            ideal_BP(n - ind_high + 1:n - ind_low) = ones(1, ind_high - ind_low);
+            filter(n - ind_high + 1:n - ind_low) = ones(1, ind_high - ind_low);
             % Downsampling
             if downsample_rate < 1
                 non_sample_num = round((1 - downsample_rate) * (ind_high - ind_low));
                 pos_non_sample_ind = randsample(ind_low:ind_high - 1, non_sample_num);
                 neg_non_sample_ind = n - pos_non_sample_ind + 1;
-                ideal_BP([pos_non_sample_ind neg_non_sample_ind]) = 0;
+                filter([pos_non_sample_ind neg_non_sample_ind]) = 0;
+            end 
+        end
+        
+        function filter = gaussian_filter(...
+                low_freq, high_freq, ... % The range of band pass
+                downsample_rate, ...     % downsampling rate
+                n, Fs)
+            % Index of low frequency and high frequency in the positive
+            % frequency domain
+            ind_low  = round(low_freq * n / Fs);
+            ind_high = round(high_freq * n / Fs);
+            filter = zeros(1, n);
+            % Gaussian window
+            sigma = (ind_high - ind_low)/6;
+            mean  = (ind_high - ind_low)/2;
+            gaussian_window = normpdf((1:(ind_high - ind_low)), mean, sigma);
+            % Positive freq part
+            filter(ind_low:ind_high - 1) = ones(1, ind_high - ind_low) .* gaussian_window; 
+            % Negative freq part
+            filter(n - ind_high + 1:n - ind_low) = ones(1, ind_high - ind_low) .* gaussian_window;
+            % Downsampling
+            if downsample_rate < 1
+                non_sample_num = round((1 - downsample_rate) * (ind_high - ind_low));
+                pos_non_sample_ind = randsample(ind_low:ind_high - 1, non_sample_num);
+                neg_non_sample_ind = n - pos_non_sample_ind + 1;
+                filter([pos_non_sample_ind neg_non_sample_ind]) = 0;
             end
             
         end
-        function ideal_BP = ideal_Postive_BP_DS_filter( ...
-                low_freq, high_freq, downsample_rate, n, Fs)
-            ind_low  = round(low_freq * n / Fs) + 1;
-            ind_high = round(high_freq * n / Fs) + 1;
-            ideal_BP = zeros(1, n);
-            % Positive freq part
-            ideal_BP(ind_low:ind_high - 1) = ones(1, ind_high - ind_low); 
-            % Negative freq part
-            % ideal_BP(n - ind_high + 1:n - ind_low) = ones(1, ind_high - ind_low);
-            % Downsampling
-            if downsample_rate < 1
-                non_sample_num = round((1 - downsample_rate) * (ind_high - ind_low));
-                pos_non_sample_ind = randsample(ind_low:ind_high - 1, non_sample_num);
-                neg_non_sample_ind = n - pos_non_sample_ind + 1;
-                % ideal_BP([pos_non_sample_ind neg_non_sample_ind]) = 0;
-                ideal_BP(pos_non_sample_ind) = 0;
-            end
-        end
-        function ideal_BP = ideal_Negative_BP_DS_filter( ...
-                low_freq, high_freq, downsample_rate, n, Fs)
-            ind_low  = round(low_freq * n / Fs) + 1;
-            ind_high = round(high_freq * n / Fs) + 1;
-            ideal_BP = zeros(1, n);
-            % Positive freq part
-            % ideal_BP(ind_low:ind_high - 1) = ones(1, ind_high - ind_low); 
-            % Negative freq part
-            ideal_BP(n - ind_high + 1:n - ind_low) = ones(1, ind_high - ind_low);
-            % Downsampling
-            if downsample_rate < 1
-                non_sample_num = round((1 - downsample_rate) * (ind_high - ind_low));
-                pos_non_sample_ind = randsample(ind_low:ind_high - 1, non_sample_num);
-                neg_non_sample_ind = n - pos_non_sample_ind + 1;
-                % ideal_BP([pos_non_sample_ind neg_non_sample_ind]) = 0;
-                ideal_BP(neg_non_sample_ind) = 0;
-            end
-        end
-        % function gaussian_BP = gaussian_BP_filter(low_freq, high_freq, n, Fs)
-            % TODO: finshi gaussian bp filter
-            
-        % end
     end
 end
